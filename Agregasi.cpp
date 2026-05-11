5676e29bc35a59cd85224833fc39776cec75d9dc
Faizaditya-oss <faizaditya750@gmail.com>
2026-05-11 17:59:06 +0700

    Menambahkan relasi agregasi antara objek ibu dan anak dengan tambahAnak()

--- Agregasi.cpp
+++ Agregasi.cpp
@@ -11,5 +11,10 @@ int main() { //agregasi
     anak* varAnak2 = new anak("rini");
     anak* varAnak3 = new anak("Dewi");
 
-    
+    varIbu->tambahAnak(varAnak1);
+    varIbu->tambahAnak(varAnak2);
+    varIbu2->tambahAnak(varAnak3);
+    varIbu2->tambahAnak(varAnak1);
+
+   
 }
