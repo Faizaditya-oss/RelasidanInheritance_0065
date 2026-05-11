0c04b6bac0ca6422587b3658033cdeaa1b32db1a
Faizaditya-oss <faizaditya750@gmail.com>
2026-05-11 18:39:21 +0700

    Constructor & Destructor class manusia.

--- manusia.h
+++ manusia.h
@@ -7,6 +7,14 @@ public:
     string name;
     jantung varJantung;
 
-    
+    manusia(string pName)
+        : name(pName) {
+        cout << name << " hidup\n";
+    }
+
+    ~manusia() {
+        cout << name << " mati\n";
+    }
 };
 
+#endif
